24e1463a83265dd9cee0b3c504cc823652f7c57e
Kevin Cai <kevincai@kevins-air.home>
2022-08-29 18:47:30 +0100

    Updated README

--- TradingAlgorithm/TestStrategy.py
+++ TradingAlgorithm/TestStrategy.py
@@ -1,24 +1,24 @@
 import math
-import backtrader as bt
+import backtesting as bt
 
 #MACD line is the FastEMA - SlowEMA
 #Signal line is the mean of the 9 day MA of the MACD line
 
 class SimpleStrategy(bt.Strategy):
-    params = (('fast',12), ('slow', 26), ('order_percentage', 0.95), ('ticker', 'DOT'))
+    params = (('fast',50), ('slow', 200), ('order_percentage', 0.95), ('ticker', 'DOT'))
     #fast-12, slow-26
 
     def __init__(self):
         self.fast_moving_average = bt.indicators.SMA(
             self.data.close,
             period=self.params.fast,
-            plotname='12 day moving average'
+            plotname='200 day moving average'
         )
 
         self.slow_moving_average = bt.indicators.SMA(
             self.data.close,
             period=self.params.slow,
-            plotname='26 day moving average'
+            plotname='50 day moving average'
         )
 
         self.crossover = bt.indicators.CrossOver(self.fast_moving_average, self.slow_moving_average)
@@ -32,7 +32,7 @@ class SimpleStrategy(bt.Strategy):
                 amount_to_invest = (self.params.order_percentage * self.broker.cash)
                 self.size = math.floor(amount_to_invest / self.data.close)
 
-                print(f"Buying {self.size} shares of {self.params.ticker} at {self.data.close[0]}")
+                #print(f"Buying {self.size} shares of {self.params.ticker} at {self.data.close[0]}")
 
                 self.buy(size=self.size)
             
@@ -40,6 +40,6 @@ class SimpleStrategy(bt.Strategy):
         if (self.position.size > 0) and (self.stoc < 50):
             #if the MA cross happens sell
             if self.crossover < 0:
-                print(f"Selling {self.size} shares of {self.params.ticker} at {self.data.close[0]}")
+                #print(f"Selling {self.size} shares of {self.params.ticker} at {self.data.close[0]}")
 
                 self.close()
