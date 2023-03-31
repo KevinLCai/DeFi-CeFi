6916bce41af47f48d2d2da6ca57770ff3b4519ae
Kevin Cai <kevincai@Kevins-MacBook-Pro.local>
2023-03-31 22:25:36 +0100

    initial pairs trading script

---
+++ TradingAlgorithm/strategies/PairsTrading.py
@@ -0,0 +1,9 @@
+import math
+import pandas as pd
+
+class PairsTrading():
+    def __init__(self, df):
+        self.df = df
+
+    def next(self, token1_price, token2_price, spread_factor):
+        pass
